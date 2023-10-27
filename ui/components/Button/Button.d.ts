c3318715ecf50ea45fd84502cdcbac0a3f1dc7c8
Dan Spratling <dan@skyward.digital>
2023-10-27 23:14:48 +0100

    Adds prev/next links

--- ui/components/Button/Button.d.ts
+++ ui/components/Button/Button.d.ts
@@ -1,4 +1,4 @@
-export interface ButtonBaseProps {
+export type ButtonBaseProps = {
   variant?: "primary" | "outline" | "ghost" | "text" | "destructive"
   size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl"
   className?: string
@@ -7,16 +7,14 @@ export interface ButtonBaseProps {
   ) => void
 }
 
-export interface ButtonLinkProps
-  extends ButtonBaseProps,
-    React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  children: string
-}
+export type ButtonLinkProps = ButtonBaseProps &
+  React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+    children: React.ReactNode
+  }
 
-export interface ButtonElementProps
-  extends ButtonBaseProps,
-    React.ButtonHTMLAttributes<HTMLButtonElement> {
-  href?: never
-}
+export type ButtonElementProps = ButtonBaseProps &
+  React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    href?: never
+  }
 
 export type ButtonProps = ButtonLinkProps | ButtonElementProps
