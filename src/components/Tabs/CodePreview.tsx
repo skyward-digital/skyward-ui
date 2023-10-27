780bf7a6e064047829138edd08492cf8e6a3116f
Dan Spratling <dan@skyward.digital>
2023-10-27 22:44:51 +0100

    Adds code previews to all current components

--- src/components/Tabs/CodePreview.tsx
+++ src/components/Tabs/CodePreview.tsx
@@ -1,12 +1,12 @@
-import { cn } from "#/utils"
+import { useState } from "react"
 import {
   TabGroup,
   TabList,
-  TabTrigger,
-  TabPanels,
   TabPanel,
+  TabPanels,
+  TabTrigger,
 } from "#/ui/components/Tabs"
-import { useState } from "react"
+import { cn } from "#/utils"
 import { renderToString } from "react-dom/server"
 
 function decodeHtml(html: string) {
@@ -16,11 +16,11 @@ function decodeHtml(html: string) {
 }
 
 export const CodePreview = ({
-  preview,
   code,
+  children,
 }: {
-  preview: React.ReactElement
   code: React.ReactElement
+  children: React.ReactElement
 }) => {
   const [copying, setCopying] = useState(false)
 
@@ -52,45 +52,53 @@ export const CodePreview = ({
                     "rounded-md px-2 py-1.5 text-xs font-medium border border-transparent duration-150",
                     selected
                       ? "bg-indigo-500 text-white border-indigo-500"
-                      : "text-gray-400 hover:text-gray-200 hover:bg-gray-400/25 dark:hover:text-gray-200 dark:hover:bg-gray-200/10"
+                      : "text-gray-400 hover:text-gray-200 hover:bg-gray-400/25 dark:hover:text-gray-200 dark:hover:bg-gray-200/10",
+                    !code &&
+                      "bg-transparent border-transparent text-gray-400 pointer-events-none"
                   )
                 }
               >
                 Preview
               </TabTrigger>
-              <TabTrigger
-                className={({ selected }: { selected: boolean }) =>
-                  cn(
-                    "rounded-md px-2 py-1.5 text-xs font-medium border border-transparent duration-150",
-                    selected
-                      ? "bg-indigo-500 text-white border-indigo-500"
-                      : "text-gray-400 hover:text-gray-700 hover:bg-gray-300/25 dark:hover:text-gray-200 dark:hover:bg-gray-200/10"
-                  )
-                }
-              >
-                Code
-              </TabTrigger>
-              <span className="w-px h-2 bg-gray-400/50" />
-              <button
-                className={cn(
-                  "rounded-md py-1.5 text-xs font-medium border border-transparent duration-150 w-[3.25rem]",
-                  selectedIndex === 0
-                    ? "text-gray-400 hover:text-gray-700 hover:bg-gray-300/25 dark:hover:text-gray-200 dark:hover:bg-gray-200/10"
-                    : "text-gray-400 hover:text-gray-200 hover:bg-gray-400/25 dark:hover:text-gray-200 dark:hover:bg-gray-200/10"
-                )}
-                onClick={copyCode}
-              >
-                {copying ? "Copied!" : "Copy"}
-              </button>
+              {code && (
+                <>
+                  <TabTrigger
+                    className={({ selected }: { selected: boolean }) =>
+                      cn(
+                        "rounded-md px-2 py-1.5 text-xs font-medium border border-transparent duration-150",
+                        selected
+                          ? "bg-indigo-500 text-white border-indigo-500"
+                          : "text-gray-400 hover:text-gray-700 hover:bg-gray-300/25 dark:hover:text-gray-200 dark:hover:bg-gray-200/10"
+                      )
+                    }
+                  >
+                    Code
+                  </TabTrigger>
+                  <span className="w-px h-2 bg-gray-400/50" />
+                  <button
+                    className={cn(
+                      "rounded-md py-1.5 text-xs font-medium border border-transparent duration-150 w-[3.25rem]",
+                      selectedIndex === 0
+                        ? "text-gray-400 hover:text-gray-700 hover:bg-gray-300/25 dark:hover:text-gray-200 dark:hover:bg-gray-200/10"
+                        : "text-gray-400 hover:text-gray-200 hover:bg-gray-400/25 dark:hover:text-gray-200 dark:hover:bg-gray-200/10"
+                    )}
+                    onClick={copyCode}
+                  >
+                    {copying ? "Copied!" : "Copy"}
+                  </button>
+                </>
+              )}
             </>
           )}
         </TabList>
 
         <TabPanels className="flex flex-1">
-          <TabPanel className="not-prose w-full h-full border border-gray-200 px-6 py-16 rounded-lg my-6 shadow-md shadow-gray-200/20">
-            {preview}
+          <TabPanel className="not-prose w-full h-full border border-gray-200 px-6 py-16 rounded-lg my-6 shadow-md flex items-center justify-center shadow-gray-200/20 gap-2 flex-wrap">
+            {children}
           </TabPanel>
-          <TabPanel className="w-full h-full [& pre]:py-8">{code}</TabPanel>
+          {code && (
+            <TabPanel className="w-full h-full [& pre]:py-8">{code}</TabPanel>
+          )}
         </TabPanels>
       </TabGroup>
     </div>
