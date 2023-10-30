0ff75d4953f28e9f1fc49cc618e74cd6de9dfea5
Dan Spratling <dan@skyward.digital>
2023-10-30 11:14:42 +0000

    Adds additional page/content types

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,7 +23,54 @@ const componentsCollection = defineCollection({
     // tags: z.array(z.string()),
   }),
 })
+
+const exampleCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    client: z.boolean().optional(),
+    links: z
+      .object({
+        github: z.string().optional(),
+        storybook: z.string().optional(),
+      })
+      .optional(),
+    // pubDate: z.date(),
+    // author: z.string(),
+    // image: z.object({
+    //   url: z.string(),
+    //   alt: z.string(),
+    // }),
+    // tags: z.array(z.string()),
+  }),
+})
+
+const pageSections = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    client: z.boolean().optional(),
+    links: z
+      .object({
+        github: z.string().optional(),
+        storybook: z.string().optional(),
+      })
+      .optional(),
+    // pubDate: z.date(),
+    // author: z.string(),
+    // image: z.object({
+    //   url: z.string(),
+    //   alt: z.string(),
+    // }),
+    // tags: z.array(z.string()),
+  }),
+})
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   components: componentsCollection,
+  example: exampleCollection,
+  pageSections: pageSections,
 }
